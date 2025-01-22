6b6f22a9bd80238b0f38c20c2407afe95547b71f
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-01-21 21:45:23 -0300

    función onclick

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = "Juego del número secreto";
 
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = "Indica un número del 1 al 10.";
+
+const intentoDeUsuario = () => {
+    alert('Click desde el botón')
+};
